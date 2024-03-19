fa66161da1a70f8a53ce6e0959cf61c42292f72f
B1229011 <144075534+Lee2406337@users.noreply.github.com>
2024-03-19 18:52:08 +0800

    Update main4.c

--- hw2/main4.c
+++ hw2/main4.c
@@ -1,43 +1,53 @@
 #include <stdio.h>
 
-typedef struct {
-    char id[100];
+typedef struct Books {
+    int id;
+    int price;
     char title[100];
-    float price;
 } book_t;
 
 void total_price(book_t books[], int num_books);
-
-void list_books(book_t books[], int num_books);
+void list_books(book_t books[], int num_books, int id);
 
 int main() {
-    book_t books[6] = {
-        {"1", "All the Light We Cannot See", 1000},
-        {"2", "The 38 Letters from J.D. Rockefeller to his son", 300},
-        {"3", "The Ballad of Songbirds and Snakes", 1000},
-        {"4", "Killers of the Flower Moon", 520},
-        {"5", "Elon Musk", 870},
-        {"6", "Milk and Honey 12-Month 2024", 1344}
+    book_t books[] = {
+        {1, 1000, "All the Light We Cannot See"},
+        {2, 300, "The 38 Letters from J.D. Rockefeller to his son"},
+        {3, 1000, "The Ballad of Songbirds and Snakes"},
+        {4, 550, "Killers of the Flower Moon"},
+        {5, 870, "Elon Musk"},
+        {6, 1344, "Milk and Honey 12-Month 2024"}
     };
+    int num_books = sizeof(books) / sizeof(books[0]);
 
-    total_price(books, 6);
+    int N;
+    scanf("%d", &N);
 
-    list_books(books, 6);
+    if (N == 0) {
+        total_price(books, num_books);
+    } else if (N >= 1 && N <= 6) {
+        list_books(books, num_books, N);
+    } else {
+        printf("無效的輸入。\n");
+    }
 
     return 0;
 }
 
 void total_price(book_t books[], int num_books) {
-    float total = 0;
+    int total = 0;
     for (int i = 0; i < num_books; ++i) {
         total += books[i].price;
     }
-    printf("總共價錢: %.2f\n", total);
+    printf("%d", total);
 }
 
-void list_books(book_t books[], int num_books) {
-    printf("id\tprice\ttitle \n");
+void list_books(book_t books[], int num_books, int id) {
     for (int i = 0; i < num_books; ++i) {
-        printf("%s\t%.2f\t%s\n", books[i].id, books[i].price, books[i].title);
+        if (books[i].id == id) {
+            printf("%d ", books[i].price);
+            printf("%s", books[i].title);
+            return;
+        }
     }
 }
